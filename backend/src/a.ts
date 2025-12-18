7fc299f60e5f21af3f6aad470b8ffd47edff94ad
Apple <apple@Apples-MacBook-Air.local>
2025-12-18 15:50:15 +0530

    implementing abstract class

--- backend/src/a.ts
+++ backend/src/a.ts
@@ -1,15 +1,24 @@
 // implementing the interface using the class.
 
-interface People {
-  name: string;
-  age: number;
-  greet():string;
-}
-
-class Employee implements People {
+abstract class People {
   name: string;
   age: number;
   constructor(a: string, b: number) {
+    this.name = a;
+    this.age = b;
+  }
+  abstract greet(): string;
+
+  hello() {
+    return "hello raja ji";
+  }
+}
+
+class Employee extends People {
+  name: string;
+  age: number;
+  constructor(a: string, b: number) {
+    super(a, b);
     this.name = a;
     this.age = b;
   }
@@ -19,6 +28,7 @@ class Employee implements People {
   }
 }
 
-const manager = new Employee("suraj",23);
-console.log(manager);
-console.log(manager.greet())
+const manager = new Employee("suraj", 23);
+
+console.log(manager.greet());
+console.log(manager.hello());
