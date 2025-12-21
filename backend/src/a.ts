db62630262a2c367663357efbcc099fcc85d10c5
Apple <apple@Apples-MacBook-Air.local>
2025-12-21 11:38:30 +0530

    learning the Exclude

--- backend/src/a.ts
+++ backend/src/a.ts
@@ -1,47 +1,10 @@
-// Map and Record
+// learning the Event
 
-// 1 Record
-/*
-  it is used to make key value pair in better way.
- */
+type Event = 'click' | 'scroll' | 'mousemove';
+type ExcludeEvent = Exclude<Event, 'scroll'>; // 'click' | 'mousemove'
 
-// interface User {
-//   name: string;
-//   id: number;
-// }
+const handleEvent = (event: ExcludeEvent) => {
+  console.log(`Handling event: ${event}`);
+};
 
-// type user = Record<string, User>;
-
-// const person: user = {
-//   "per1": { name: "suraj", id: 45 },
-//   "per2": { name: "suraj", id: 45 },
-// };
-
-// 2 Map
-
-/*
-  it is used to make key value pair in better way.
- */
-
-// const userMap = new Map();
-
-// userMap.set("per1", { name: "suraj", id: 49 });
-// userMap.set("per2", { name: "sharma", id: 19 });
-// userMap.set("per3", { name: "rohit" });
-
-// console.log(userMap.get("per1"));
-
-// when we fix the structure in Map
-
-interface User {
-  name: string;
-  id: number;
-}
-
-const userMap = new Map<string, User>();
-
-userMap.set("per1", { name: "suraj", id: 49 });
-userMap.set("per2", { name: "sharma", id: 19 });
-// userMap.set("per3", { name: "rohit" });  // this will show the error.
-
-console.log(userMap.get("per1"));
+handleEvent('click'); // OK
