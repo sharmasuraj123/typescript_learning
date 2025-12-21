b373832fed34589aab15fdf1751bb349ca31185f
Apple <apple@Apples-MacBook-Air.local>
2025-12-21 22:22:09 +0530

    learning the type infer in ts backend

--- backend/dist/a.js
+++ backend/dist/a.js
@@ -1,9 +1,30 @@
 "use strict";
-// Map and Record
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const userMap = new Map();
-userMap.set("per1", { name: "suraj", id: 49 });
-userMap.set("per2", { name: "sharma", id: 19 });
-// userMap.set("per3", { name: "rohit" });  // this will show the error.
-console.log(userMap.get("per1"));
+const express_1 = __importDefault(require("express"));
+const zod_1 = require("zod");
+const app = (0, express_1.default)();
+const userProfileSchema = zod_1.z.object({
+    name: zod_1.z.string().min(2, { message: "at least two characters required" }),
+    email: zod_1.z.string().email({ message: "invalid email format" }),
+    age: zod_1.z.number().min(18, { message: "age must be at least 18" }),
+});
+app.use(express_1.default.json());
+app.put("/user", (req, res) => {
+    const { success } = userProfileSchema.safeParse(req.body);
+    // const updateBody: fianlUserSchema  = req.body;
+    if (!success) {
+        res.status(411).json({});
+        return;
+    }
+    // update database here
+    res.json({
+        message: "User updated",
+    });
+});
+app.listen(3000, () => {
+    console.log("the server is running on the port 3000");
+});
 //# sourceMappingURL=a.js.map
--- backend/src/a.ts
+++ backend/src/a.ts
@@ -1,10 +1,30 @@
-// learning the Event
+import express from "express";
+import { z } from "zod";
 
-type Event = 'click' | 'scroll' | 'mousemove';
-type ExcludeEvent = Exclude<Event, 'scroll'>; // 'click' | 'mousemove'
+const app = express();
 
-const handleEvent = (event: ExcludeEvent) => {
-  console.log(`Handling event: ${event}`);
-};
+const userProfileSchema = z.object({
+  name: z.string().min(2, { message: "at least two characters required" }),
+  email: z.string().email({ message: "invalid email format" }),
+  age: z.number().min(18, { message: "age must be at least 18" }),
+});
 
-handleEvent('click'); // OK
+type fianlUserSchema = z.infer<typeof userProfileSchema>;
+app.use(express.json());
+app.put("/user", (req, res) => {
+  const { success } = userProfileSchema.safeParse(req.body);
+  // const updateBody: fianlUserSchema  = req.body;
+
+  if (!success) {
+    res.status(411).json({});
+    return;
+  }
+  // update database here
+  res.json({
+    message: "User updated",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("the server is running on the port 3000");
+});
